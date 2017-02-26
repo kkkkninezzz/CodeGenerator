d1fda76c23125717874d0f0468765ab256ca2076
kurisu <18581489108@163.com>
2017-02-26 19:20:34 +0800

    生成器入口

---
+++ src/cn/makisekurisu/codeGenerator/main/Main.java
@@ -0,0 +1,32 @@
+package cn.makisekurisu.codeGenerator.main;
+
+import cn.makisekurisu.codeGenerator.service.main.ICodeGeneratorService;
+import cn.makisekurisu.codeGenerator.service.main.generator.classFile.impl.DtoGenerator;
+import cn.makisekurisu.codeGenerator.service.main.generator.classFile.impl.ModelGenerator;
+import cn.makisekurisu.codeGenerator.service.main.generator.mapper.MapperGenerator;
+import cn.makisekurisu.codeGenerator.service.main.generator.mapperXml.MapperXmlGenerator;
+import cn.makisekurisu.codeGenerator.service.main.generator.util.UtilGenerator;
+import cn.makisekurisu.codeGenerator.service.main.impl.CodeGeneratorService;
+
+/**
+ * Created by ym on 2017/2/26 0026.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ICodeGeneratorService codeGeneratorService = new CodeGeneratorService();
+        addGenerators(codeGeneratorService);
+
+        codeGeneratorService.start();
+    }
+
+    /**
+     * 添加代码生成器
+     * */
+    private static void addGenerators(ICodeGeneratorService codeGeneratorService) {
+        codeGeneratorService.addGenerator(new ModelGenerator());
+        codeGeneratorService.addGenerator(new DtoGenerator());
+        codeGeneratorService.addGenerator(new MapperGenerator());
+        codeGeneratorService.addGenerator(new MapperXmlGenerator());
+        codeGeneratorService.addGenerator(new UtilGenerator());
+    }
+}
