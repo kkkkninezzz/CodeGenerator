bf782d479b7b1c8868bc59efff0bb7b79e08066e
kurisu <18581489108@163.com>
2017-02-26 19:06:29 +0800

    model的java文件生成器

---
+++ src/cn/makisekurisu/codeGenerator/service/main/generator/classFile/JavaBeanGenerator.java
@@ -0,0 +1,89 @@
+package cn.makisekurisu.codeGenerator.service.main.generator.classFile;
+
+import cn.makisekurisu.codeGenerator.bean.FieldInfo;
+import cn.makisekurisu.codeGenerator.bean.ModelInfo;
+import cn.makisekurisu.codeGenerator.bean.codeFile.classFile.ClassFileInfo;
+import cn.makisekurisu.codeGenerator.bean.codeFile.classFile.MethodInfo;
+import cn.makisekurisu.codeGenerator.bean.codeFile.classFile.VariableInfo;
+import cn.makisekurisu.codeGenerator.config.CodeGeneratorConfig;
+import cn.makisekurisu.util.StringUtil;
+import cn.makisekurisu.util.TypeUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ym on 2017/2/22 0022.
+ */
+public abstract class JavaBeanGenerator extends AbstractClassFileGenerator {
+    /**
+     * 生成bean的属性
+     * */
+    protected VariableInfo generateBeanProperty(FieldInfo fieldInfo) {
+        VariableInfo variableInfo = new VariableInfo();
+
+        variableInfo.setComment(fieldInfo.getComment());
+        variableInfo.setAccessControlModifier(PRIVATE_ACM);
+        variableInfo.setType(fieldInfo.getJavaType().getSimpleName());
+        variableInfo.setName(fieldInfo.getFieldName());
+
+        return variableInfo;
+    }
+
+    /**
+     * 生成bean的setter方法
+     * */
+    protected MethodInfo generateBeanSetterMehthod(VariableInfo variableInfo) {
+        MethodInfo setter = new MethodInfo();
+
+        setter.setAccessControlModifier(PUBLIC_ACM);
+        setter.setType(VOID_RETURN_TYPE);
+        setter.setName("set".concat(StringUtil.toUppercaseForFirstLetter(variableInfo.getName())));
+        setter.addArgument(String.format(ARGUMENT_FORMAT, variableInfo.getType(), variableInfo.getName()));
+        setter.setMethodBody(String.format(SETTER_FORMAT, variableInfo.getName(), variableInfo.getName()));
+
+        return setter;
+    }
+
+    /**
+     * 生成bean的getter方法
+     * */
+    protected MethodInfo generateBeanGetterMehthod(VariableInfo variableInfo) {
+        MethodInfo getter = new MethodInfo();
+
+        getter.setAccessControlModifier(PUBLIC_ACM);
+        getter.setType(variableInfo.getType());
+        getter.setName("get".concat(StringUtil.toUppercaseForFirstLetter(variableInfo.getName())));
+        getter.setMethodBody(String.format(GETTER_FORMAT, variableInfo.getName()));
+
+        return getter;
+    }
+
+
+    protected List<? extends ClassFileInfo> createCodeFiles(List<ModelInfo> modelInfos, CodeGeneratorConfig config) {
+        List<ClassFileInfo> classFileInfos = new ArrayList<ClassFileInfo>();
+
+        for(ModelInfo modelInfo : modelInfos) {
+            classFileInfos.add(toClassFileInfo(modelInfo, config));
+        }
+
+        return classFileInfos;
+    }
+
+
+
+    protected abstract ClassFileInfo toClassFileInfo(ModelInfo modelInfo, CodeGeneratorConfig config);
+
+
+    protected void handleFieldInfo(ClassFileInfo classFileInfo, FieldInfo fieldInfo) {
+        Class<?> fieldType = fieldInfo.getJavaType();
+        if(!TypeUtil.isInLangPackage(fieldType))
+            classFileInfo.addImportInfo(fieldType.getTypeName());
+
+        VariableInfo variableInfo = generateBeanProperty(fieldInfo);
+        classFileInfo.addVariableInfo(variableInfo);
+        classFileInfo.addMethodInfo(generateBeanGetterMehthod(variableInfo));
+        classFileInfo.addMethodInfo(generateBeanSetterMehthod(variableInfo));
+    }
+
+}
---
+++ src/cn/makisekurisu/codeGenerator/service/main/generator/classFile/impl/ModelGenerator.java
@@ -0,0 +1,48 @@
+package cn.makisekurisu.codeGenerator.service.main.generator.classFile.impl;
+
+import cn.makisekurisu.codeGenerator.bean.FieldInfo;
+import cn.makisekurisu.codeGenerator.bean.ModelInfo;
+import cn.makisekurisu.codeGenerator.bean.codeFile.classFile.ClassFileInfo;
+import cn.makisekurisu.codeGenerator.config.CodeGeneratorConfig;
+import cn.makisekurisu.codeGenerator.config.ConfigLoader;
+import cn.makisekurisu.codeGenerator.service.main.generator.classFile.JavaBeanGenerator;
+
+import java.util.List;
+
+/**
+ * Created by ym on 2017/2/22 0022.
+ *
+ * model的代码生成器
+ */
+public class ModelGenerator extends JavaBeanGenerator {
+
+    @Override
+    protected boolean whetherToGenerateCode(List<ModelInfo> modelInfos, ConfigLoader configLoader) {
+        // 需要生成model
+        return configLoader.getCodeGeneratorConfig().getModelFlag() && modelInfos!= null && !modelInfos.isEmpty();
+    }
+
+    @Override
+    protected ClassFileInfo toClassFileInfo(ModelInfo modelInfo, CodeGeneratorConfig config) {
+        ClassFileInfo classFileInfo = new ClassFileInfo();
+
+        classFileInfo.setPackageName(modelInfo.getPackageName());
+        classFileInfo.setComment(modelInfo.getComment());
+        classFileInfo.setAccessControlModifier(PUBLIC_ACM);
+        classFileInfo.setClassName(modelInfo.getModelName());
+        classFileInfo.setFileName(String.format(JAVA_FILE_NAME_FORMAT, classFileInfo.getClassName()));
+
+        // 默认实现Serializable接口
+        implementSerializable(classFileInfo);
+
+        // 生成属性
+        for(FieldInfo fieldInfo : modelInfo.getFieldInfos()) {
+            handleFieldInfo(classFileInfo, fieldInfo);
+        }
+
+
+        return classFileInfo;
+    }
+
+
+}
