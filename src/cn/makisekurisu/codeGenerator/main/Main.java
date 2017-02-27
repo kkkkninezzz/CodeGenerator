2f81043b9056474ac16a54d2c9cbf5f28f660096
kurisu <18581489108@163.com>
2017-02-27 22:36:35 +0800

    更新main函数

--- src/cn/makisekurisu/codeGenerator/main/Main.java
+++ src/cn/makisekurisu/codeGenerator/main/Main.java
@@ -3,6 +3,7 @@ package cn.makisekurisu.codeGenerator.main;
 import cn.makisekurisu.codeGenerator.service.main.ICodeGeneratorService;
 import cn.makisekurisu.codeGenerator.service.main.generator.classFile.impl.DtoGenerator;
 import cn.makisekurisu.codeGenerator.service.main.generator.classFile.impl.ModelGenerator;
+import cn.makisekurisu.codeGenerator.service.main.generator.iservice.IServiceGenerator;
 import cn.makisekurisu.codeGenerator.service.main.generator.mapper.MapperGenerator;
 import cn.makisekurisu.codeGenerator.service.main.generator.mapperXml.MapperXmlGenerator;
 import cn.makisekurisu.codeGenerator.service.main.generator.util.UtilGenerator;
@@ -28,5 +29,6 @@ public class Main {
         codeGeneratorService.addGenerator(new MapperGenerator());
         codeGeneratorService.addGenerator(new MapperXmlGenerator());
         codeGeneratorService.addGenerator(new UtilGenerator());
+        codeGeneratorService.addGenerator(new IServiceGenerator());
     }
 }
